5afd8044d37d369808d7a67c9263b4233d18bad7
Mithrandir0x <mithrandir0x@gmail.com>
2011-09-29 09:57:49 +0300

    Corrected several things over P2 python file.

--- Algorismica/P2/exercici2.py
+++ Algorismica/P2/exercici2.py
@@ -1,6 +1,8 @@
 # exercici2.py
 #
-# author: mithrandir0x
+# author: olopezsa13
+
+import math
 
 def futval():
     print "Aquest programa calcula el valor futur d'una determinada inversio a una quantitat d'anys desitjada."
@@ -13,15 +15,16 @@ def futval():
 
 def convert():
     print "Taula d'equivalencies Celsius a Fahrenheit"
-    for celsius in range(0, 100, 10):
+    for celsius in range(0, 110, 10):
         fahrenheit = 9.0 / 5.0 * celsius + 32
         print "Celsius:", celsius, " | Fahrenheit:", fahrenheit
 
 def exp():
     print "(a)", 4.0 / 10.0 + 3.5 * 2
-    print "(b)", 10 % 4 + 6 / 2
+    print "(b)", float(10 % 4 + 6) / 2
     print "(c)", abs(4 - 20 / 3) ** 3
-    print "(d)", sqrt(4.5 - 5.0) + 7 * 3
+    #print "(d)", math.sqrt(4.5 - 5.0) + 7 * 3
+    print "(d)", math.sqrt(5.0 -4.5) + 7 * 3
     print "(e)", 3 * 10 / 3 + 10 % 3
     print "(f)", 3L ** 3
 
@@ -33,20 +36,31 @@ def punts():
     else:
         print "La pendent que dona la recta que passa per aquests 2 punts es:", ( y2 - y2 ) / ( x2 - x1 )
 
-def euclid():
+def euclid_real():
     print "Calcul de la distancia euclidiana entre dos punts"
     x1, y1, x2, y2 = input("Donem els punts en format 'x1, y1, x2, y2': ")
     inner = ( ( x2 - x1 ) ** 2 ) + ( ( y2 - y1 ) ** 2 )
+    res = math.sqrt(inner)
     if inner < 0:
         print "No es pot calcular."
-    else:
-        print "La distancia es:", sqrt(inner)
+    return res
+
+def euclid():
+    res = euclid_real()
+    print "La distancia euclidiana es:", res
 
 def euclid2():
-    print "El mateix que l'anterior"
+    res = euclid_real()
+    print "La distancia euclidiana es:", int(res)
 
 def factmenor():
-    print "Nivell d'inteligencia insuficient per fer aquest exercici."
+    val = 6204484017332394393600000
+    n = 1   # Valor del factorial
+    cal = 1 # Resultat actual del calcul del factorial
+    while cal < val:
+        cal = cal * n
+        n = n + 1
+    print n
 
 def suma():
     print "Suma dels nombres menors de 100 que son multiples de 3 i 5"
@@ -54,7 +68,9 @@ def suma():
     for i in range(100):
         if i % 3 == 0 or i % 5 == 0:
             sum += i
+        print i, i % 3, i % 5, sum
     print "El nombre es:", sum
 
 def divisible():
-     print "Calcul del nombre natural mes petit que es divisible per 2, 3, 4, 5, 6, 7, 8, 9, i 10"
+    print "Calcul del nombre natural mes petit que es divisible per 2, 3, 4, 5, 6, 7, 8, 9, i 10"
+     print "Calcul del nombre natural mes petit que es divisible per 2, 3, 4, 5, 6, 7, 8, 9, i 10"print "Calcul del nombre natural mes petit que es divisible per 2, 3, 4, 5, 6, 7, 8, 9, i 10"
