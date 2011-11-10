6bdd7f03463e1fe6021a94c8012a1b551fcb441c
mithrandir0x <mithrandir0x@gmail.com>
2011-11-10 17:50:03 +0100

    Added more implementation of exercici6.py.

--- Algorismica/P6/exercici6.py
+++ Algorismica/P6/exercici6.py
@@ -4,24 +4,20 @@
 # autor: olopezsa13
 #
 
-def levenshtein(first, second):
-    if len(first) > len(second):
-        first, second = second, first
-    if len(second) == 0:
-        return len(first)
-    lFirst = len(first) + 1
-    lSecond = len(second) + 1
-    distanceMatrix = [[0] * lSecond for x in range(lFirst)]
-    for j in range(lSecond): distanceMatrix[0][j] = j
-    for i in xrange(1, lFirst):
-        for j in range(1, lSecond):
-            deletion = distanceMatrix[i-1][j] + 1
-            insertion = distanceMatrix[i][j-1] + 1
+def levenshtein(text, pattern):
+    lnText = len(text) + 1
+    lnPattern = len(pattern) + 1
+    distanceMatrix = [[0] * lnPattern for x in range(lnText)]
+    for j in range(lnPattern): distanceMatrix[0][j] = j
+    for i in xrange(1, lnText):
+        for j in range(1, lnPattern):
+            deletion = distanceMatrix[i-1][j] + 2
+            insertion = distanceMatrix[i][j-1] + 2
             substitution = distanceMatrix[i-1][j-1]
-            if first[i-1] != second[j-1]:
+            if text[i-1] != pattern[j-1]:
                 substitution += 1
-                distanceMatrix[i][j] = min(insertion, deletion, substitution)
-    return distanceMatrix[lFirst-1][lSecond-1]
+            distanceMatrix[i][j] = min(insertion, deletion, substitution)
+    return distanceMatrix
 
 def llegirCromosoma(fitxer):
     try:
@@ -34,10 +30,16 @@ def llegirCromosoma(fitxer):
         return []
 
 def cercaGenetica():
-    cadenes = [ 
+    cadenes = [
         'AGATACATTAGACAATAGAGATGTGGTC',
         'GTCAGTCTGGCCTTGCCATTGGTGCCACCA',
         'TACCGAGAAGCTGGATTACAGCATGTACCATCAT' ]
     cromosoma2 = llegirCromosoma('HUMAN-DNA.txt')
+    for cadena in cadenes:
+        for fragment in cromosoma2:
+            dm = levenshtein(fragment, cadena)
+    matriu = levenshtein('C ABBA C', 'DDD')
+    print matriu
 
+cercaGenetica()
 
