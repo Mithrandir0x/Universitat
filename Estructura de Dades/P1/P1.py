a6fa63e17616761a1ce51500a74cd91258347e3f
Mithrandir0x <mithrandir0x@gmail.com>
2012-02-12 08:19:39 +0100

    No tabs while I leave, die you miserable tab.

--- Estructura de Dades/P1/P1.py
+++ Estructura de Dades/P1/P1.py
@@ -5,19 +5,19 @@
 #
 
 def parser():
-	database = []
-	filmEntries = readFilmsFile("peliculas100.dat")
-	for entry in filmEntries:
-		databaseEntry = []
-		fields = entry.split("|")
-		for field in fields:
-			if ( field.find("&&") ):
-				composedField = field.split("&&")
-				databaseEntry.append(composedField)
-			else:
-				databaseEntry.append(field)
-		database.append(databaseEntry)
-	return database
+    database = []
+    filmEntries = readFilmsFile("peliculas100.dat")
+    for entry in filmEntries:
+        databaseEntry = []
+        fields = entry.split("|")
+        for field in fields:
+            if ( field.find("&&") ):
+                composedField = field.split("&&")
+                databaseEntry.append(composedField)
+            else:
+                databaseEntry.append(field)
+        database.append(databaseEntry)
+    return database
 
 def readFilmsFile(fileName):
     try:
